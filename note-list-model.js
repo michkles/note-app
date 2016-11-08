5ca86d1247921ad340a6dcff7d6e83cb4087a689
glynester <glynester@hotmail.com>
2016-11-08 10:16:52 +0000

    Some corrections but NoteList is not defined - still!

--- note-list-model.js
+++ note-list-model.js
@@ -1,7 +1,11 @@
 (function(exports) {
-  function NoteList(note){
-    this.notes = [note];
+  function NoteList(){
+    this.notes = [];
   };
+
+  Notelist.prototype.addNote = function(note){
+    this.notes.push(note);
+  }
 
   NoteList.prototype.returnNotes = function(){
     return this.notes;
--- test/unit/note-list-model-spec.js
+++ test/unit/note-list-model-spec.js
@@ -1,9 +1,11 @@
 function testNoteListModel(){
-  var testNoteList = new NoteList("My favourite language is JavaScript");
+  var testNoteList = new NoteList();
+  testNoteList.addNote("My favourite language is JavaScript");
   if(testNoteList.returnNotes() != "XMy favourite language is JavaScript"){
     throw new Error ("Note is not there");
   } else {
     console.log("Success : " + testNoteList.returnNotes)
   }
-
 }
+
+testNoteListModel();
