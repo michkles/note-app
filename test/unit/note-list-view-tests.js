771c5c1e822810736350c073ba402afe21a9ecf5
michkles <michkles@gmail.com>
2016-11-09 11:47:32 +0000

    added test for returning html

--- test/unit/note-list-view-tests.js
+++ test/unit/note-list-view-tests.js
@@ -4,4 +4,14 @@ function testNoteListViewInstantiation() {
     assert.isTrue( noteListView.noteLists() === noteList );
 }
 
+function testNoteListViewReturningHtml() {
+  var noteList = new NoteList();
+  noteList.newNote('Favourite beer: Heineken');
+  noteList.newNote('Favourite editor: Vim');
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Heineken</div></li><li><div>Favourite editor: Vim</div></li></ul>");
+}
+
+
 testNoteListViewInstantiation();
+testNoteListViewReturningHtml();
