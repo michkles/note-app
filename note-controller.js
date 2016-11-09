2f46fb6d9f34d68b72bc7f1b40f175abe8aae255
Amaal Ali <amq.ali@gmail.com>
2016-11-09 17:22:05 +0000

    Controller shows a note in the index.html page.

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,22 @@
-var elem = document.getElementById("app");
-console.log(elem.innerHTML);
-elem.innerHTML = "howdy";
+// var elem = document.getElementById("app");
+// console.log(elem.innerHTML);
+// elem.innerHTML = "howdy";
+
+(function(exports) {
+
+    function NoteController(notelist) {
+      this.noteList = notelist || new NoteList();
+      this.view = new NoteListView(notelist);
+    }
+
+    NoteController.prototype.newNote = function(text) {
+      this.noteList.newNote(text);
+    }
+
+    NoteController.prototype.printHtml= function() {
+      var elem = document.getElementById("app");
+      elem.innerHTML = this.view.printHtml();
+    }
+
+  exports.NoteController = NoteController;
+})(this);
