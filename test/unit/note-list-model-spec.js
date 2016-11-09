1e78f8ed251bccb2e0c067c266ad80e5ea852e4d
michkles <michkles@gmail.com>
2016-11-09 10:55:58 +0000

    finished challenge 2

--- test/unit/note-list-model-spec.js
+++ test/unit/note-list-model-spec.js
@@ -2,7 +2,14 @@ function testNoteListInstantiation(){
   var noteList = new NoteList();
   var note = new Note("This is a Note!");
   noteList.addNote(note);
-  assert.isTrue(noteList.returnNoteText(0) === note.returnText());
+  assert.isTrue(noteList.returnNoteList().includes(note));
+}
+
+function testNoteListCreation (){
+  var notelist = new NoteList();
+  notelist.newNote('buy beer');
+  assert.isTrue(notelist.notes[0].text === 'buy beer');
 }
 
 testNoteListInstantiation();
+testNoteListCreation();
