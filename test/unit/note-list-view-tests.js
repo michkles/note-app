563ebf84c469251f88ec8bf5c76f7c4b416ff244
michkles <michkles@gmail.com>
2016-11-10 16:38:28 +0000

    step8 completed

--- test/unit/note-list-view-tests.js
+++ test/unit/note-list-view-tests.js
@@ -8,12 +8,21 @@ function testNoteListViewReturningHtml() {
   var noteList = new NoteList();
   var noteListView = new NoteListView(noteList);
   assert.isTrue( noteListView.printHtml() === "<ul></ul>");
-  noteList.newNote('Favourite beer: Heineken');
-  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Heineken</div></li></ul>");
-  noteList.newNote('Favourite editor: Vim');
-  assert.isTrue( noteListView.printHtml() === "<ul><li><div>Favourite beer: Heineken</div></li><li><div>Favourite editor: Vim</div></li></ul>");
+  noteList.newNote('beer: Heineken');
+  assert.isTrue( noteListView.printHtml() === "<ul><li><div>beer: Heineken</div></li></ul>");
+  noteList.newNote('editor: Vim');
+  assert.isTrue( noteListView.printHtml() === "<ul><li><div>beer: Heineken</div></li><li><div>editor: Vim</div></li></ul>");
 }
 
+function testNoteListViewLessThanTwenty() {
+  var noteList = new NoteList();
+  var noteListView = new NoteListView(noteList);
+  noteList.newNote('I need to buy 999999 bottles of rum');
+  var print = noteListView.printHtml();
+  var tags = "<ul><li><div></div></li></ul>"
+  assert.isTrue( (print.length) <= (20 + tags.length));
+}
 
 testNoteListViewInstantiation();
 testNoteListViewReturningHtml();
+testNoteListViewLessThanTwenty();
